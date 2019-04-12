1f58f3836273386faa1409c149aa1f14dcd4e231
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2019-04-12 18:27:36 +0200

    Creating StandardRules

---
+++ PicariaWebApp/PicariaWebApp/Game/IRules.cs
@@ -0,0 +1,14 @@
+﻿using PicariaWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicariaWebApp.Game
+{
+    interface IRules
+    {
+        List<Position> GetPossibleMovesOfPawn(List<Position> board, Position pawnPostion);
+        //List<Tuple<Position, Position>> GetPossibleMovesOfPlayer();
+    }
+}
---
+++ PicariaWebApp/PicariaWebApp/Game/StandardRules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PicariaWebApp.Models;
+
+namespace PicariaWebApp.Game
+{
+    public class StandardRules : IRules
+    {
+        public List<Position> GetPossibleMovesOfPawn(List<Position> board, Position pawnPostion)
+        {
+            List<Position> ret = new List<Position>();
+            foreach (Position e in board)
+            {
+                if (e.Status == Status.FreeToCapture && (e.X == pawnPostion.X || e.X == pawnPostion.X - 1 || e.X == pawnPostion.X + 1) &&
+                    (e.Y == pawnPostion.Y || e.Y == pawnPostion.Y - 1 || e.Y == pawnPostion.Y + 1))
+                {
+                    ret.Add(e);
+                }
+            }
+            return ret;
+        }
+    }
+}
