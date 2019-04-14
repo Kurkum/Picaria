8ab881fad6f58536478a991912a2651dcce838e4
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2019-04-14 14:18:49 +0200

    Board class

--- PicariaWebApp/PicariaWebApp/Models/Board.cs
+++ PicariaWebApp/PicariaWebApp/Models/Board.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PicariaWebApp.Game;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,40 @@ namespace PicariaWebApp.Models
 {
     public class Board
     {
-        public string Board2 { get; set; }
+        public List<Position> Positions { get; set; }
+
+        Board()
+        {
+            Positions = new List<Position>();
+        }
+
+        public void RealizeMove(Move move)
+        {
+            move.NewPosition.Status = move.OldPosition.Status;
+            move.OldPosition.Status = Status.FreeToCapture;
+        }
+        public Board GetCopyOfBoardWithMoveRealized(Move move)
+        {
+            Board board = new Board();
+            Position oldPosiotion = move.OldPosition.Clone();
+            Position newPosition = move.NewPosition.Clone();
+            newPosition.Status = oldPosiotion.Status;
+            oldPosiotion.Status = Status.FreeToCapture;
+            foreach(Position position in Positions)
+            {
+                if (position.HaveSameCoordinates(oldPosiotion))
+                {
+                    board.Positions.Add(oldPosiotion);
+                }
+                else if (position.HaveSameCoordinates(newPosition))
+                {
+                    board.Positions.Add(newPosition);
+                }
+                else {
+                    board.Positions.Add(position.Clone());
+                }
+            }
+            return board;
+        }
     }
 }
--- PicariaWebApp/PicariaWebApp/Models/Position.cs
+++ PicariaWebApp/PicariaWebApp/Models/Position.cs
@@ -10,6 +10,20 @@ namespace PicariaWebApp.Models
         public int X { get; set; }
         public int Y { get; set; }
         public Status Status { get; set; }
+
+        public Position Clone()
+        {
+            Position clone = new Position();
+            clone.Status = Status;
+            clone.X = X;
+            clone.Y = Y;
+            return clone;
+        }
+
+        public bool HaveSameCoordinates(Position position)
+        {
+            return (X == position.X && Y == position.Y);
+        }
     }
 
     public enum Status
