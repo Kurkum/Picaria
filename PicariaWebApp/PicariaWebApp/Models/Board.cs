f2b80dac9b7f3dff71ef5734e13a958947678c55
Kurkum <34893110+Kurkum@users.noreply.github.com>
2019-05-11 13:32:57 +0200

    Revert "Merge branch 'master' into Rating"
    This reverts commit a0ccd968bd0edb47ba53b1ed1a9c97bde82adc1f, reversing
    changes made to 0c664ab747c0491200c17416da4cf2605048cc97.

--- PicariaWebApp/PicariaWebApp/Models/Board.cs
+++ PicariaWebApp/PicariaWebApp/Models/Board.cs
@@ -11,45 +11,17 @@ namespace PicariaWebApp.Models
         public List<Position> Positions { get; set; }
         public IRules Rules;
 
-        public static List<Position> GetEmptyBoard()
-        {
-            return new List<Position>()
-            {
-                new Position(0,0),
-                new Position(1,0),
-                new Position(2,0),
-
-                new Position(0,1),
-                new Position(1,1),
-                new Position(2,1),
-
-                new Position(0,2),
-                new Position(1,2),
-                new Position(2,2)
-            };
-        }
-
-        public Board()
+        Board()
         {
             Positions = new List<Position>();
         }
 
-        public Board(List<Position> positions)
-        {
-            foreach(var position in positions)
-            {
-                position.TranslatePosition();
-            }
-
-            Positions = positions;
-        }
-
-        public void ExecuteMove(Move move)
+        public void RealizeMove(Move move)
         {
             move.NewPosition.Status = move.OldPosition.Status;
             move.OldPosition.Status = Status.FreeToCapture;
         }
-        public Board GetCopyOfBoardWithMoveExecuted(Move move)
+        public Board GetCopyOfBoardWithMoveRealized(Move move)
         {
             Board board = new Board();
             Position oldPosiotion = move.OldPosition.Clone();
@@ -58,11 +30,11 @@ namespace PicariaWebApp.Models
             oldPosiotion.Status = Status.FreeToCapture;
             foreach(Position position in Positions)
             {
-                if (position.Equals(oldPosiotion))
+                if (position.HaveSameCoordinates(oldPosiotion))
                 {
                     board.Positions.Add(oldPosiotion);
                 }
-                else if (position.Equals(newPosition))
+                else if (position.HaveSameCoordinates(newPosition))
                 {
                     board.Positions.Add(newPosition);
                 }
--- PicariaWebApp/PicariaWebApp/Models/Position.cs
+++ PicariaWebApp/PicariaWebApp/Models/Position.cs
@@ -11,58 +11,18 @@ namespace PicariaWebApp.Models
         public int Y { get; set; }
         public Status Status { get; set; }
 
-        private Dictionary<int, int> XPositionToRealPosition = new Dictionary<int, int>
-        {
-            {50,  0},
-            {400, 1},
-            {750, 2}
-        };
-        private Dictionary<int, int> YPositionToRealPosition = new Dictionary<int, int>
-        {
-            {50,  0},
-            {300, 1},
-            {550, 2}
-        };
-
-        public void TranslatePosition()
-        {
-            this.X = XPositionToRealPosition[this.X];
-            this.Y = YPositionToRealPosition[this.Y];
-        }
-        public Position()
-        {
-
-        }
-
-        public Position(int x, int y, Status status)
-        {
-            X = x;
-            Y = y;
-            Status = status;
-        }
-
-        public Position(int x, int y)
-        {
-            X = x;
-            Y = y;
-            Status = Status.FreeToCapture;
-        }
-
         public Position Clone()
         {
-            return this.MemberwiseClone() as Position;
+            Position clone = new Position();
+            clone.Status = Status;
+            clone.X = X;
+            clone.Y = Y;
+            return clone;
         }
 
-
-        public override bool Equals(object obj)
+        public bool HaveSameCoordinates(Position position)
         {
-            if(obj is Position)
-            {
-                var castedObj = obj as Position;
-                return (X == castedObj.X && Y == castedObj.Y);
-            }
-
-            return base.Equals(obj);
+            return (X == position.X && Y == position.Y);
         }
     }
 
