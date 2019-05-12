a9aae058b889a9ae10d0c2cd25c2517f49d36665
Kurkum <34893110+Kurkum@users.noreply.github.com>
2019-05-12 10:43:20 +0200

    Mała zmiana przed mergem

---
+++ PicariaWebApp/RatingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RatingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RatingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1db68ea5-c364-4ea3-9ab3-26044a37f22a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PicariaWebApp/RatingTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RatingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRate()
+        {
+            List<Position> positions = new List<Positions> { }
+            positions.Add(new Position)
+            int i = PicariaWebApp.Player.Rating.RateBoard();
+            bool value = true;
+            Assert.IsTrue(value);
+        }
+    }
+}
