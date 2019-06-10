0bb86b24b9626699fb0f59891af20304f13b033e
przemyslawczajka <przemyslawczajka@users.noreply.github.com>
2019-06-10 11:07:47 +0200

    GameResultChecker

---
+++ PicariaWebApp/PicariaWebApp/Models/GameResultChecker.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PicariaWebApp.Models
+{
+    public class GameResultChecker
+    {
+        static public GameResult CheckGameResult(Board board)
+        {
+            GameResult result;
+            result = _checkResultHorizontally(board);
+            if (result != GameResult.Other)
+            {
+                return result;
+            }
+            result = _checkResultVertically(board);
+            if (result != GameResult.Other)
+            {
+                return result;
+            }
+            result = _checkResultDiagonally(board);
+            if (result != GameResult.Other)
+            {
+                return result;
+            }
+            return GameResult.Other;
+        }
+
+        static private GameResult _checkResultHorizontally(Board board)
+        {
+            Status checkedStatus;
+            for (int y = 0; y < 3; ++y)
+            {
+                checkedStatus = board.PositionAt(0, y).Status;
+                if (checkedStatus == Status.FreeToCapture)
+                {
+                    continue;
+                }
+                for (int x = 0; x < 3; ++x)
+                {
+                    if (checkedStatus != board.PositionAt(x, y).Status)
+                    {
+                        break;
+                    }
+                    else if (x == 2)
+                    {
+                        if (checkedStatus == Status.PlayerOne)
+                        {
+                            return GameResult.PlayerOneWon;
+                        }
+                        else
+                        {
+                            return GameResult.PlayerTwoWon;
+                        }
+                    }
+                }
+            }
+            return GameResult.Other;
+        }
+
+        static private GameResult _checkResultVertically(Board board)
+        {
+            Status checkedStatus;
+            for (int x = 0; x < 3; ++x)
+            {
+                checkedStatus = board.PositionAt(x, 0).Status;
+                if (checkedStatus == Status.FreeToCapture)
+                {
+                    continue;
+                }
+                for (int y = 0; y < 3; ++y)
+                {
+                    if (checkedStatus != board.PositionAt(x, y).Status)
+                    {
+                        break;
+                    }
+                    else if (y == 2)
+                    {
+                        if (checkedStatus == Status.PlayerOne)
+                        {
+                            return GameResult.PlayerOneWon;
+                        }
+                        else
+                        {
+                            return GameResult.PlayerTwoWon;
+                        }
+                    }
+                }
+            }
+            return GameResult.Other;
+        }
+
+        static private GameResult _checkResultDiagonally(Board board)
+        {
+            if (board.PositionAt(0, 0).Status == board.PositionAt(1, 1).Status &&
+                board.PositionAt(1, 1).Status == board.PositionAt(2, 2).Status)
+            {
+                if (board.PositionAt(0, 0).Status == Status.PlayerOne)
+                {
+                    return GameResult.PlayerOneWon;
+                }
+                else if (board.PositionAt(0, 0).Status == Status.PlayerTwo)
+                {
+                    return GameResult.PlayerTwoWon;
+                }
+            }
+            if (board.PositionAt(2, 0).Status == board.PositionAt(1, 1).Status &&
+                board.PositionAt(1, 1).Status == board.PositionAt(0, 2).Status)
+            {
+
+                if (board.PositionAt(2, 0).Status == Status.PlayerOne)
+                {
+                    return GameResult.PlayerOneWon;
+                }
+                else if (board.PositionAt(2, 0).Status == Status.PlayerTwo)
+                {
+                    return GameResult.PlayerTwoWon;
+                }
+            }
+            return GameResult.Other;
+        }
+    }
+    public enum GameResult
+    {
+        PlayerOneWon, PlayerTwoWon, Other
+    }
+}
--- PicariaWebApp/PicariaWebApp/Player/SimpleArtificialIntelligence.cs
+++ PicariaWebApp/PicariaWebApp/Player/SimpleArtificialIntelligence.cs
@@ -23,13 +23,13 @@ namespace PicariaWebApp.Player
             Board boardOnWhichIOccupyMiddlePoint = null;
             foreach (GameTree child in gameTree.Children)
             {
-                if (IfIWin(CheckGameResult(child.BoardState)))
+                if (IfIWin(GameResultChecker.CheckGameResult(child.BoardState)))
                 {
                     return child.BoardState;
                 }
                 foreach (GameTree child2 in child.Children)
                 {
-                    if (IfILoose(CheckGameResult(child2.BoardState)))
+                    if (IfILoose(GameResultChecker.CheckGameResult(child2.BoardState)))
                     {
                         goto GoFurther;
                     }
@@ -87,124 +87,5 @@ namespace PicariaWebApp.Player
             }
         }
 
-        public GameResult CheckGameResult(Board board)
-        {
-            GameResult result;
-            result = _checkResultHorizontally(board);
-            if (result != GameResult.Other)
-            {
-                return result;
-            }
-            result = _checkResultVertically(board);
-            if (result != GameResult.Other)
-            {
-                return result;
-            }
-            result = _checkResultDiagonally(board);
-            if (result != GameResult.Other)
-            {
-                return result;
-            }
-            return GameResult.Other;
-        }
-
-        private GameResult _checkResultHorizontally(Board board)
-        {
-            Status checkedStatus;
-            for (int y = 0; y < 3; ++y)
-            {
-                checkedStatus = board.PositionAt(0, y).Status;
-                if (checkedStatus == Status.FreeToCapture)
-                {
-                    continue;
-                }
-                for (int x = 0; x < 3; ++x)
-                {
-                    if (checkedStatus != board.PositionAt(x, y).Status)
-                    {
-                        break;
-                    }
-                    else if (x == 2)
-                    {
-                        if (checkedStatus == Status.PlayerOne)
-                        {
-                            return GameResult.PlayerOneWon;
-                        }
-                        else
-                        {
-                            return GameResult.PlayerTwoWon;
-                        }
-                    }
-                }
-            }
-            return GameResult.Other;
-        }
-
-        private GameResult _checkResultVertically(Board board)
-        {
-            Status checkedStatus;
-            for (int x = 0; x < 3; ++x)
-            {
-                checkedStatus = board.PositionAt(x, 0).Status;
-                if (checkedStatus == Status.FreeToCapture)
-                {
-                    continue;
-                }
-                for (int y = 0; y < 3; ++y)
-                {
-                    if (checkedStatus != board.PositionAt(x, y).Status)
-                    {
-                        break;
-                    }
-                    else if (y == 2)
-                    {
-                        if (checkedStatus == Status.PlayerOne)
-                        {
-                            return GameResult.PlayerOneWon;
-                        }
-                        else
-                        {
-                            return GameResult.PlayerTwoWon;
-                        }
-                    }
-                }
-            }
-            return GameResult.Other;
-        }
-
-        private GameResult _checkResultDiagonally(Board board)
-        {
-            if (board.PositionAt(0, 0).Status == board.PositionAt(1, 1).Status &&
-                board.PositionAt(1, 1).Status == board.PositionAt(2, 2).Status)
-            {
-                if (board.PositionAt(0, 0).Status == Status.PlayerOne)
-                {
-                    return GameResult.PlayerOneWon;
-                }
-                else if (board.PositionAt(0, 0).Status == Status.PlayerTwo)
-                {
-                    return GameResult.PlayerTwoWon;
-                }
-            }
-            if (board.PositionAt(2, 0).Status == board.PositionAt(1, 1).Status &&
-                board.PositionAt(1, 1).Status == board.PositionAt(0, 2).Status)
-            {
-
-                if (board.PositionAt(2, 0).Status == Status.PlayerOne)
-                {
-                    return GameResult.PlayerOneWon;
-                }
-                else if (board.PositionAt(2, 0).Status == Status.PlayerTwo)
-                {
-                    return GameResult.PlayerTwoWon;
-                }
-            }
-            return GameResult.Other;
-        }
-
-    }
-    public enum GameResult
-    {
-        PlayerOneWon, PlayerTwoWon, Other
     }
 }
